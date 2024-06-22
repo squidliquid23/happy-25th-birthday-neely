03a255f844f3e19fac5d6e55c7db4db6469b42cf
Tyler Turner <meruemval@outlook.com>
2024-06-21 23:16:18 -0400

    Initial commit

---
+++ js/javascript_learning.js
@@ -0,0 +1,7 @@
+// create a new 'Date' object
+const now = new Date();
+
+// get the current date and time as a string
+const currentDateTime = now.toLocaleString();
+
+console.log(currentDateTime); //output: xx/xx/xxxx, x:xx:xx AM/PM
