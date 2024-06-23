3ea2098e5642979ffc64574347721c094fbe2703
Tyler Turner <meruemval@outlook.com>
2024-06-23 00:44:47 -0400

    giant changes

--- js/javascript_learning.js
+++ js/javascript_learning.js
@@ -1,7 +1,19 @@
-// create a new 'Date' object
-const now = new Date();
+const timeElement = document.querySelector(".time");
+const dateElement = document.querySelector(".date");
 
-// get the current date and time as a string
-const currentDateTime = now.toLocaleString();
 
-console.log(currentDateTime); //output: xx/xx/xxxx, x:xx:xx AM/PM
+function formatTime(date) {
+    const hours12 = date.getHours() % 12 || 12;
+    const minutes = date.getMinutes();
+    const isAm = date.getHours() < 12;
+    
+    return `${hours12.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")} ${isAm ? "AM" : "PM"}`;
+}
+
+
+function formatDate(date) {
+    const DAYS = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const MONTHS = ["January", "February", "March", "April", "June", "July", "August", "September", "October", "November", "December"];
+    
+    return `${DAYS[date.getDay()]}, ${MONTHS[date.getMonth()]} ${date.getDate()} ${date.getFullYear()}`;
+}
